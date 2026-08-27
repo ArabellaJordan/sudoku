6316b4479124831e3d23dcb5a4010d3aeda4a108
arabella <jordanfaearabella@gmail.com>
2026-08-27 20:18:43 +0800

    fill board with number between 1-9

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { checkRow } from "./index.js";
+import { checkRow, generateBoard } from "./index.js";
 
 describe("checkRow", () => {
   it("returns true when the number exists in the row", () => {
@@ -14,5 +14,24 @@ describe("checkRow", () => {
     const result = checkRow(9, row);
 
     expect(result).toBe(false);
+  });
+});
+
+/**
+ * Checks if every cell of the board is a number between 1 and 9
+ * Checks if the numbers in a row are unique
+ * Checks if the numbers in a column are unique
+ * Checks if the numbers in a 3x3 box are unique
+ */
+describe("generateBoard", () => {
+  it("returns true if every number in a cell is between 1 and 9", () => {
+    const result = generateBoard();
+
+    for (const row of result) {
+      for (const cell of row) {
+        expect(cell).toBeGreaterThanOrEqual(1);
+        expect(cell).toBeLessThanOrEqual(9);
+      }
+    }
   });
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 let board: number[][] = [];
 
-function generateSolution() {
+export function generateBoard() {
   board = initializeBoard();
 
   const selection = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -14,17 +14,10 @@ function generateSolution() {
       board![row]![col] = updatedSelection[index]!;
       updatedSelection.splice(index, 1);
     }
-    // console.log("board: ", board);
   }
-
-  //   console.log("check row", checkRow(2, 0));
+  console.log(board);
+  return board;
 }
-
-// generate random number between 1 and 9
-// function getRandomNumber(): number {
-//   const number = Math.floor(Math.random() * 9);
-//   return number;
-// }
 
 // checks if the row includes the input
 export function checkRow(number: number, row: number[]): boolean {
@@ -32,19 +25,8 @@ export function checkRow(number: number, row: number[]): boolean {
   return exist;
 }
 
-// function generateRow(): number[] {
-//   const selection = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//   const row: number[] = [];
-//   for (let n = 1; n <= 9; n++) {
-//     const index = Math.floor(Math.random() * selection.length);
-//     row.push(selection[index]);
-//     selection.splice(index, 1);
-//   }
-//   return row;
-// }
-
 function initializeBoard() {
   return Array.from({ length: 9 }, () => Array(9).fill(0));
 }
 
-generateSolution();
+generateBoard();
