c9694a1692e568e0eadd5aae41c2857995dae778
arabella <ara03bella@gmail.com>
2026-09-09 23:51:28 +0800

    only show a few numbers in the board

--- index.ts
+++ index.ts
@@ -2,9 +2,26 @@ import { solvedBoard } from "./solution.js";
 let board: number[][] = [];
 
 export function generateBoard() {
-  board = getBoard();
+  const solvedBoard = getBoard();
+  const validBoard = showFewNumbers(solvedBoard);
 
-  console.log("board: ", board);
+  console.log("board: ", validBoard);
+
+  return board;
+}
+
+// only show a few numbers
+function showFewNumbers(completeBoard: number[][]) {
+  const board = completeBoard.map((row) => row.map(() => 0));
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const showNumber = Math.random() < 0.3;
+      if (showNumber) {
+        board[row]![col] = completeBoard[row]![col]!;
+      }
+    }
+  }
+
   return board;
 }
 
