82105c535291acd06183f40874c7b0073fd0b329
arabella <ara03bella@gmail.com>
2026-09-09 23:15:36 +0800

    load a presolved board

--- index.ts
+++ index.ts
@@ -1,25 +1,10 @@
+import { solvedBoard } from "./solution.js";
 let board: number[][] = [];
 
 export function generateBoard() {
-  board = initializeBoard();
+  board = getBoard();
 
-  const selection = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let updatedSelection: number[];
-  for (let row = 0; row < 9; row++) {
-    updatedSelection = [...selection];
-
-    for (let col = 0; col < 9; col++) {
-      let index: number;
-      const column = board.map((row) => row[col]);
-      do {
-        index = Math.floor(Math.random() * updatedSelection.length);
-      } while (existInColumn(updatedSelection[index]!, column));
-
-      board![row]![col] = updatedSelection[index]!;
-      updatedSelection.splice(index, 1);
-    }
-  }
-  console.log(board);
+  console.log("board: ", board);
   return board;
 }
 
@@ -34,8 +19,8 @@ export function checkRow(number: number, row: number[]): boolean {
   return exist;
 }
 
-function initializeBoard() {
-  return Array.from({ length: 9 }, () => Array(9).fill(0));
+function getBoard() {
+  return solvedBoard;
 }
 
 generateBoard();
