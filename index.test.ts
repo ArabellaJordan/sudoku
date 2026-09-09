2c1936211b4a3240d946a6a8c1617ac1298eaa8b
arabella <ara03bella@gmail.com>
2026-09-10 00:23:03 +0800

    remove: test for generating board

--- index.test.ts
+++ index.test.ts
@@ -16,39 +16,3 @@ describe("checkRow", () => {
     expect(result).toBe(false);
   });
 });
-
-/**
- * Checks if every cell of the board is a number between 1 and 9
- * Checks if the numbers in a row are unique
- * Checks if the numbers in a column are unique
- * Checks if the numbers in a 3x3 box are unique
- */
-describe("generateBoard", () => {
-  const result = generateBoard();
-
-  it("returns true if every number in a cell is between 1 and 9", () => {
-    for (const row of result) {
-      for (const cell of row) {
-        expect(cell).toBeGreaterThanOrEqual(1);
-        expect(cell).toBeLessThanOrEqual(9);
-      }
-    }
-  });
-
-  it("returns true if the numbers in each row is unique", () => {
-    for (const row of result) {
-      expect(new Set(row).size).toBe(9);
-    }
-  });
-
-  it("returns true if the numbers in each column is unique", () => {
-    for (let col = 0; col < 9; col++) {
-      const column = result.map((row) => row[col]);
-
-      console.log("board: ", result);
-      console.log("column: ", column);
-
-      expect(new Set(column).size).toBe(9);
-    }
-  });
-});
